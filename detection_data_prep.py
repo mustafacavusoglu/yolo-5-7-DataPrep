ef5f954a2475bdb85b40836be07224b4e49380f3
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2022-12-26 08:57:33 +0300

    çoklu bbox bulma ve tersine çevirme kodu eklendi

--- detection_data_prep.py
+++ detection_data_prep.py
@@ -2,15 +2,25 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def convert_2(xmin, w_img, ymin, h_img, w, h):
+def convert(xmin, w, ymin, h, w_img, h_img):
     xcenter = (xmin + w/2) / w_img
     ycenter = (ymin + h/2) / h_img
     w = w / w_img
     h = h / h_img
 
     return xcenter, ycenter, w, h
+
+
+def reverse_convert(xcenter, ycenter, w, h, w_img, h_img):
+    h = h * h_img
+    w = w * w_img
+    x = (xcenter * w_img) - w/2
+    y = (ycenter * h_img) - h/2
+
+    return x, y, w + x,  y + h
 
 
 def yolobbox2bbox(x,y,w,h):
@@ -25,13 +35,18 @@ def bbox1(img):
     bbox = np.min(a[1]), np.max(a[1]), np.min(a[0]), np.max(a[0])
     return bbox
 
+def find_bbox(img:np.array) -> list:
+    "find bbox using masks"
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # threshold
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
 
-for i in images:
-    img = cv2.imread(i)
-    xmin, xmax, ymin, ymax = bbox1(img)
-    x, y, w, h = convert(img.shape, (xmin, xmax, ymin, ymax))
-    print(x, y, w, h)
-    txt_path = i.replace('masks', 'images').replace('.png', '.txt')
-    with open(txt_path, 'w') as txt:
-        txt.write('0 ' + str(x) + ' ' + str(y) + ' ' +  str(w) + ' ' +  str(h))
-
+    # get contour bounding boxes and draw on copy of input
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    bboxes = []
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        bboxes.append([x,y,w,h])
+    
+    return bboxes
